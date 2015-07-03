c8bc81cbaaa7edb3b36f799a857b38ba490bb754
vixlar <modelo@iMac-01.local>
2015-07-03 14:23:44 -0500

    tarea 07

---
+++ SidebarMenu/ReportesViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ReportesViewController.swift
+//  SidebarMenu
+//
+//  Created by modelo on 03/07/15.
+//  Copyright (c) 2015 Unimodelo. All rights reserved.
+//
+
+import UIKit
+
+class ReportesViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
